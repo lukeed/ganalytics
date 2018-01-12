a5189fe1678482b83af2d679373e0196fe8a0f02
Luke Edwards <luke.edwards05@gmail.com>
2018-01-12 08:37:42 -0800

    chore: update `strict` location for rollup

--- builder.js
+++ builder.js
@@ -8,28 +8,30 @@ const pkg = require('./package');
 const umd = pkg['umd:main'];
 
 rollup({
-	strict: false,
 	input: 'src/index.js'
 }).then(bun => {
 	bun.write({
 		format: 'cjs',
 		file: pkg.main,
-		exports: 'default'
+		exports: 'default',
+		strict: false
 	});
 
 	bun.write({
 		format: 'es',
 		file: pkg.module,
-		exports: 'default'
+		exports: 'default',
+		strict: false
 	});
 
 	bun.write({
 		file:umd,
 		format: 'umd',
 		exports: 'default',
-		name: pkg['umd:name']
+		name: pkg['umd:name'],
+		strict: false
 	}).then(_ => {
-		const data = fs.readFileSync(pkg.main, 'utf8');
+		const data = fs.readFileSync(umd, 'utf8');
 
 		// produce minified output
 		const { code } = minify(data, { fromString:true });
