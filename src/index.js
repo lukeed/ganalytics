dc003bc21ada5798248837775c98b85ff7e416bb
Luke Edwards <luke.edwards05@gmail.com>
2018-10-13 12:13:03 -0700

    refactor: inline `encode` helper

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 var KEY = 'ga:user';
 var UID = (localStorage[KEY] = localStorage[KEY] || Math.random() + '.' + Math.random());
 
-// modified `obj-str`
-function encode(obj) {
-	var k, str='https://www.google-analytics.com/collect?v=1';
-	for (k in obj) {
-		if (obj[k]) {
-			str += ('&' + k + '=' + encodeURIComponent(obj[k]));
-		}
-	}
-	return str;
-}
-
 function GA(ua, opts, toWait) {
 	opts = opts || {};
 	this.args = Object.assign({ tid:ua, cid:UID }, opts);
@@ -23,7 +12,12 @@ GA.prototype.send = function (type, opts) {
 		opts = { dl:location.href, dt:document.title };
 	}
 	var obj = Object.assign({ t:type }, this.args, opts, { z:Date.now() });
-	new Image().src = encode(obj); // dispatch a GET
-}
+	var k, str='https://www.google-analytics.com/collect?v=1';
+	for (k in obj) {
+		// modified `obj-str` behavior
+		if (obj[k]) str += ('&' + k + '=' + encodeURIComponent(obj[k]));
+	}
+	new Image().src = str; // dispatch a GET
+};
 
 export default GA;
