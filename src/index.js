1a88e25c3f37602e93151c5c93a76c06d981f545
Jamie Kyle <me@thejameskyle.com>
2022-07-19 15:13:12 -0700

    String concat is shorter than template string

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ export default (ua, args, toWait) => {
 	};
 
 	let send = (type, opts = type === 'pageview' && { dl:location.href, dt:document.title }) => {
-		fetch(`https://www.google-analytics.com/collect?${new URLSearchParams({
+		fetch('https://www.google-analytics.com/collect?' + new URLSearchParams({
 			t: type,
 			...args,
 			...opts,
 			z: +Date(),
 			v: 1,
-		})}`);
+		}));
 	};
 
 	toWait || send('pageview');
